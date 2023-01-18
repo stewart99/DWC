ac5548e76f36935cf16b0bd361adddfcae445788
stewart99 <85374519+stewart99@users.noreply.github.com>
2023-01-19 00:44:57 +0100

    Completado la comprobación si las cartas son iguales o diferentes

--- juegoMemoria_v2/html/js/juegoMemoria.js
+++ juegoMemoria_v2/html/js/juegoMemoria.js
@@ -1,88 +1,87 @@
-
 class Tabla{
 
 
-        constructor(filas,columnas){
+    constructor(filas,columnas){
 
-            this.filas=filas;
-            this.columnas=columnas;
+        this.filas=filas;
+        this.columnas=columnas;
 
-            this.crearTablero();
-        }
+        this.crearTablero();
+    }
 
 
 
-    Comprobacion(){
+Comprobacion(){
 
-        this.filas=prompt("Dime el numero de filas");
-        this.columnas=prompt("Dime el numero de columnas");
-        
+    this.filas=prompt("Dime el numero de filas");
+    this.columnas=prompt("Dime el numero de columnas");
     
 
-        if((this.filas * this.columnas) % 2 != 0 || this.filas <= 0 || this.columnas <=0){
+
+    if((this.filas * this.columnas) % 2 != 0 || this.filas <= 0 || this.columnas <=0){
 
 
 
-            alert("El numero introducido de filas o de las columnas es impar");
-    
-            this.Comprobacion();
-    
-        }
-    
-    
+        alert("El numero introducido de filas o de las columnas es impar");
+
+        this.Comprobacion();
 
     }
-        
-        crearTablero(){
-
-            this.tablaMemoria=[];
-
-            for (let fila = 0; fila < this.filas; fila++) {
-               
-                this.tablaMemoria[fila]=[];
-            
-            for (let  columna = 0; columna < this.columnas; columna++) {
-              
 
 
-                this.tablaMemoria[fila][columna]="";
 
+}
+    
+    crearTablero(){
+
+        this.tablaMemoria=[];
+
+        for (let fila = 0; fila < this.filas; fila++) {
            
-            }
+            this.tablaMemoria[fila]=[];
+        
+        for (let  columna = 0; columna < this.columnas; columna++) {
+          
 
 
-                
-            }
+            this.tablaMemoria[fila][columna]="";
+
+       
+        }
 
 
             
-
         }
 
 
-
-        pintarTablaMemoria(){
-
-            document.write('<table>');
-
-        for (let i = 0; i < this.filas; i++) {
-            document.write('<tr>');
         
-        for (let j = 0; j < this.columnas; j++) {
-           
-            document.write("<td><img src=" + this.tablaMemoria[i][j] +  "></td>");
-            
-        }
+
+    }
+
+
+
+    pintarTablaMemoria(){
+
+        document.write('<table>');
+
+    for (let i = 0; i < this.filas; i++) {
+        document.write('<tr>');
+    
+    for (let j = 0; j < this.columnas; j++) {
+       
+        document.write("<td><img src=" + this.tablaMemoria[i][j] +  "></td>");
         
-        document.write('</tr>');
-        }
+    }
+    
+    document.write('</tr>');
+    }
 
 
 
-        document.write('</table>');
+    document.write('</table>');
 
 
-        }
+    }
 
 
 
@@ -93,30 +92,30 @@ class Tabla{
 dibujarTableroDOM(){
 
 
-    let tabla = document.createElement('table');
-    let fila;
-    let columna;
+let tabla = document.createElement('table');
+let fila;
+let columna;
 
 
-    for (let i = 0; i < this.filas; i++) {
-        
-        fila = document.createElement('tr');
-        tabla.appendChild(fila);
-
+for (let i = 0; i < this.filas; i++) {
     
-    for (let j = 0; j < this.columnas; j++) {
-        
-        columna = document.createElement('td');
-        columna.id = `f${i}_c${j}`;
-        columna.dataset.fila=i;
-        columna.dataset.columna = j;
-        fila.appendChild(columna);
-        
-    }
-        
-    }
+    fila = document.createElement('tr');
+    tabla.appendChild(fila);
 
-    document.body.appendChild(tabla);
+
+for (let j = 0; j < this.columnas; j++) {
+    
+    columna = document.createElement('td');
+    columna.id = `f${i}_c${j}`;
+    columna.dataset.fila=i;
+    columna.dataset.columna = j;
+    fila.appendChild(columna);
+    
+}
+    
+}
+
+document.body.appendChild(tabla);
 
 }
 
@@ -125,196 +124,213 @@ dibujarTableroDOM(){
 class JuegoMemoria extends Tabla{
 
 
-        constructor(filas,columnas){
+    constructor(filas,columnas){
 
-            super(filas,columnas);
-            this.comprobar;
-            this.contadorClick=0;
-            this.dibujarTableroDOM();
-           
-
-        }
-
-
-    
-        colocarParejas(){
-
-
-    let parejas=["img/mario.png","img/caparazon.png","img/goomba.png","img/koopa.png","img/lakitu.png","img/luigi.png","img/luma.png","img/luna.png","img/mario.png","img/seta.png"];
-    let posFila=0;
-    let posColumna=0;
-    let contadorNumparejas=0;
-    let contadorParejas=0;
-    let repeticion=true;
-    let numeroCasillas=(this.filas * this.columnas)/2;
-
-
-    while(contadorNumparejas < numeroCasillas){
-
-        posFila=Math.floor(Math.random()*this.filas);
-        posColumna=Math.floor(Math.random()*this.columnas);
-
-    
-
-        if(this.tablaMemoria[posFila][posColumna] == ""){
-
-
-            this.tablaMemoria[posFila][posColumna]=parejas[contadorParejas];
-
-
-        
-            while(repeticion){
-
-                posFila=Math.floor(Math.random()*this.filas);
-                posColumna=Math.floor(Math.random()*this.columnas);
-        
-        
-        
-                if(this.tablaMemoria[posFila][posColumna] == ""){
-        
-        
-                    this.tablaMemoria[posFila][posColumna]=parejas[contadorParejas];
-                    repeticion=false;
-        
-                
-    
-                }
-
-    
-        }
+        super(filas,columnas);
+        this.comprobar;
+        this.idultimacelda;
+        this.contadorClick=0;
+        this.dibujarTableroDOM();
        
-        contadorNumparejas++;
-        contadorParejas++;
-        repeticion=true;
 
-
-
-        if(contadorParejas == 10){
-
-
-            contadorParejas = 0;
-        }
-    }  
-        
     }
+
+
+
+    colocarParejas(){
+
+
+let parejas=["img/mario.png","img/caparazon.png","img/goomba.png","img/koopa.png","img/lakitu.png","img/luigi.png","img/luma.png","img/luna.png","img/mario.png","img/seta.png"];
+let posFila=0;
+let posColumna=0;
+let contadorNumparejas=0;
+let contadorParejas=0;
+let repeticion=true;
+let numeroCasillas=(this.filas * this.columnas)/2;
+
+
+while(contadorNumparejas < numeroCasillas){
+
+    posFila=Math.floor(Math.random()*this.filas);
+    posColumna=Math.floor(Math.random()*this.columnas);
+
+
+
+    if(this.tablaMemoria[posFila][posColumna] == ""){
+
+
+        this.tablaMemoria[posFila][posColumna]=parejas[contadorParejas];
+
 
     
-    }
+        while(repeticion){
 
-
-
-    dibujarTableroDOM(){
-        
-
-        super.dibujarTableroDOM();
-       
-        let celda;
-
-        this.destapar = this.destapar.bind(this);
-
-
-    for (let i = 0; i < this.filas; i++) {
-        
-        for (let j = 0; j < this.columnas; j++) {
-           
-            celda=document.getElementById(`f${i}_c${j}`);
+            posFila=Math.floor(Math.random()*this.filas);
+            posColumna=Math.floor(Math.random()*this.columnas);
+    
+    
+    
+            if(this.tablaMemoria[posFila][posColumna] == ""){
+    
+    
+                this.tablaMemoria[posFila][posColumna]=parejas[contadorParejas];
+                repeticion=false;
+    
             
-            celda.addEventListener('click',this.destapar);
-            
-        }
-        
-    }
-
-    }
-
-
-    destapar(elEvento){
-
-        let evento = elEvento || window.event;
-        let celda = evento.currentTarget;
-        let fila = celda.dataset.fila;
-        let columna=celda.dataset.columna;
-        let valorCelda = this.tablaMemoria[fila][columna];
-        let  imagen = document.createElement('img');
-        imagen.src=valorCelda;
-        let rutaimagen=imagen.src.split('/').slice(-2).join('/');
-
-      
-
-        if(celda.lastChild == null){
-
-        
-
-            celda.appendChild(imagen);
-
-            this.contadorClick++;
-
-            if(rutaimagen != this.comprobar && this.comprobar != null){
-
-
-                 console.log("disntito"); 
-               
-                celda.removechild(imagen);
-
-            }else if(rutaimagen == this.comprobar && this.comprobar != null){
-
-                console.log("iguales"); 
-
-
 
             }
+
+
+    }
+   
+    contadorNumparejas++;
+    contadorParejas++;
+    repeticion=true;
+
+
+
+    if(contadorParejas == 10){
+
+
+        contadorParejas = 0;
+    }
+}  
+    
+}
+
+
+}
+
+
+
+dibujarTableroDOM(){
+    
+
+    super.dibujarTableroDOM();
+   
+    let celda;
+
+    this.destapar = this.destapar.bind(this);
+
+
+for (let i = 0; i < this.filas; i++) {
+    
+    for (let j = 0; j < this.columnas; j++) {
+       
+        celda=document.getElementById(`f${i}_c${j}`);
+        
+        celda.addEventListener('click',this.destapar);
+        
+    }
+    
+}
+
+}
+
+
+destapar(elEvento){
+
+    let evento = elEvento || window.event;
+    let celda = evento.currentTarget;
+    let fila = celda.dataset.fila;
+    let columna=celda.dataset.columna;
+    let idcelda=celda.id;
+    let valorCelda = this.tablaMemoria[fila][columna];
+    let  imagen = document.createElement('img');
+    imagen.src=valorCelda;
+    let ultimafoto = document.getElementById(this.idultimacelda);
+   
+  
+    
+
+    if(celda.lastChild == null){
+
+    
+
+        celda.appendChild(imagen);
+
+        this.contadorClick++;
+
+      
+     
+      
+
+        if(this.contadorClick == 2){
+
+        if(this.idultimacelda != idcelda && this.idultimacelda != null){
+
+
+            if(this.ultimafoto != valorCelda){
+     
+             console.log("distinto"); 
+
+
+             setTimeout(borrar,5000);
+
+             function borrar(){
+
+
+             celda.removeChild(celda.lastChild);
+             ultimafoto.removeChild(ultimafoto.lastChild);
+
+
+             }
+               
+           
+        }else{
+
+            console.log("iguales");
+        
+
+    }
 
          
-          
-            if(this.contadorClick == 2){
+    }
 
+ 
+    console.log(this.tablaMemoria)
 
-                this.contadorClick=0;
+    if(this.contadorClick == 2){
 
+        this.contadorClick=0;
 
-            }
+    }
+}
 
-  
-           console.log(this.contadorClick);
-           
-        }
-
-
-    
-
-        
-        this.comprobar=valorCelda;
-        
-
-
-        
-  
-
-     
-   
-        
-
-
-      
     }
 
 
 
+    this.idultimacelda=idcelda;
+    this.ultimafoto=valorCelda;
     
+
+
+    
+
+
+ 
+
+    
+
+
+  
+}
+
+
+
+
 
 }
 
 
 
 window.onload = function() {
-    let buscaminas1 = new JuegoMemoria(2,2); 
-    buscaminas1.crearTablero();
-    buscaminas1.colocarParejas();
-    
+let buscaminas1 = new JuegoMemoria(2,2); 
+buscaminas1.crearTablero();
+buscaminas1.colocarParejas();
+
 }
-
-
-
 
 
 
